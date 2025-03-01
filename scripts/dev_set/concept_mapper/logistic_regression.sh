4e755bcfb10761f184cf1d7fcb28fd8f828be6ea
Vedant Pungliya <vedant29@iastate.edu>
2025-02-28 20:44:29 -0600

    Dev runs

--- scripts/dev_set/concept_mapper/logistic_regression.sh
+++ scripts/dev_set/concept_mapper/logistic_regression.sh
@@ -3,10 +3,10 @@
 
 scriptDir=src/concept_mapper
 fileDir=results/dev/position_representation_info  #saliency_representation_info #
-classifierDir=results/dev/classifier_mapping/result/model
+classifierDir=results/train/classifier_mapping/models/model
 
-mkdir "latent_concepts"
-mkdir "latent_concepts/position_prediction"
+mkdir "results/dev/latent_concepts"
+mkdir "results/dev/latent_concepts/position_prediction"
 
 
 layer=12
@@ -14,7 +14,7 @@ echo ${layer}
 python ${scriptDir}/logistic_regression.py \
   --test_file_path ${fileDir}/explanation_words_representation_layer${layer}.csv \
   --layer ${layer} \
-  --save_path ./latent_concepts/position_prediction/ \
+  --save_path ./results/dev/latent_concepts/position_prediction/ \
   --do_predict \
   --classifier_file_path $classifierDir/layer_${layer}_classifier.pkl \
   --load_classifier_from_local
--- scripts/dev_set/concept_mapper/match_representation.sh
+++ scripts/dev_set/concept_mapper/match_representation.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 scriptDir=src/concept_mapper
-input=movie_dev.txt.tok.sent_len
-working_file=$input.tok.sent_len
+input=filtered
+working_file=$input
+layer=12
 
-dataPath=results/dev/clustering
-minfreq=0
-maxfreq=1000000
+dataPath=results/dev/clustering/layer${layer}
+minfreq=5
+maxfreq=20
 delfreq=1000000
 
-savePath=rposition_representation_info
+savePath=results/dev/position_representation_info
 mkdir $savePath
 
-layer=12
 saveFile=$savePath/explanation_words_representation_layer${layer}.csv
 explanation=results/dev/CLS_explanation/explanation_CLS.txt
-python ${scriptDir}/match_representation.py --datasetFile $dataPath/layer$layer/${working_file}-layer${layer}_min_${minfreq}_max_${maxfreq}_del_${delfreq}-dataset.json --explanationFile $explanation --outputFile $saveFile
+python ${scriptDir}/match_representation.py --datasetFile $dataPath/dataset/${working_file}_min_${minfreq}_max_${maxfreq}_del_${delfreq}-dataset.json --explanationFile $explanation --outputFile $saveFile
